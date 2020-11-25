0b1392a5de97bab47d29f3efbc890b749c04b9b3
Ashutosh <ashutosh.singh.de@gmail.com>
2020-11-25 17:26:35 +0530

    Start Adding neural network class

---
+++ deeplearning/exercise-1/src_to_implement/NeuralNetwork.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class NeuralNetwork(object):
+    def __init__(self, optimizer, loss, layers, data_layer, loss_layer):
+        pass
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
+
+    def append_trainable_layer(self):
+        pass
+
+    def train(self):
+        pass
+
+    def test(self):
+        pass
