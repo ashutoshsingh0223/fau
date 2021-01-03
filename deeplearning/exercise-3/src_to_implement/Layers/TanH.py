e0bd1800e68a7697c1b87d96d3d1212c9842a8b4
Ashutosh <ashutosh.singh.de@gmail.com>
2021-01-03 14:36:10 +0530

    Add Activation functions - sigmoid and tanh

---
+++ deeplearning/exercise-3/src_to_implement/Layers/TanH.py
@@ -0,0 +1,15 @@
+import numpy as np
+from Layers.Base import BaseLayer
+
+
+class TanH(BaseLayer):
+    def __init__(self):
+        super(TanH, self).__init__()
+        self.activations = None
+
+    def forward(self, input_tensor):
+        self.activations = np.tanh(input_tensor)
+        return self.activations
+
+    def backward(self, error_tensor):
+        return np.multiply(error_tensor, np.subtract(1, np.square(self.activations)))
